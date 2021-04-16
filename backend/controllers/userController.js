ad9a37c6c78a418fb885ec8e10d84b1117385457
Kundan Patel <kundanpatel@kundans-mbp.myfiosgateway.com>
2021-04-16 02:18:36 -0400

    Front end authentication (login page)

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,7 @@ import asyncHandler from 'express-async-handler'
 import generateToken from '../utils/generateToken.js'
 import User from '../models/userModel.js'
 
+
 // @desc auth user and get token
 // @route POST api/users/login
 // @access Public
@@ -24,6 +25,7 @@ const authUser = asyncHandler(async (req, res) => {
 		throw new Error('Invalid email or password')
 	}
 })
+
 
 // @desc Register a new user
 // @route POST api/users
@@ -58,6 +60,7 @@ const registerUser = asyncHandler(async (req, res) => {
 		throw new Error('Invalid user data')
 	}
 })
+
 
 // @desc get user profile
 // @route GET api/users/profile
