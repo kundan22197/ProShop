cf4f2bc3ec8a4194289e17508d863d6094d13ac8
Kundan Patel <kundanpatel@kundans-mbp.myfiosgateway.com>
2021-04-18 00:16:59 -0400

    Front end user authentication with login, sign up and profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,7 +2,6 @@ import asyncHandler from 'express-async-handler'
 import generateToken from '../utils/generateToken.js'
 import User from '../models/userModel.js'
 
-
 // @desc auth user and get token
 // @route POST api/users/login
 // @access Public
@@ -25,7 +24,6 @@ const authUser = asyncHandler(async (req, res) => {
 		throw new Error('Invalid email or password')
 	}
 })
-
 
 // @desc Register a new user
 // @route POST api/users
@@ -61,7 +59,6 @@ const registerUser = asyncHandler(async (req, res) => {
 	}
 })
 
-
 // @desc get user profile
 // @route GET api/users/profile
 // @access Private
@@ -82,4 +79,33 @@ const getUserProfile = asyncHandler(async (req, res) => {
 	}
 })
 
-export { authUser, registerUser, getUserProfile }
+// @desc Update user profile
+// @route PUT api/users/profile
+// @access Private
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+	const user = await User.findById(req.user._id)
+
+	if (user) {
+		user.name = req.body.name || user.name
+		user.email = req.body.email || user.email
+		if (req.body.password) {
+			user.password = req.body.password
+		}
+
+		const updatedUser = await user.save()
+
+		res.json({
+			_id: updatedUser._id,
+			name: updatedUser.name,
+			email: updatedUser.email,
+			isAdmin: updatedUser.isAdmin,
+			token: generateToken(updatedUser._id)
+		})
+	} else {
+		res.status(404)
+		throw new Error('User not found')
+	}
+})
+
+export { authUser, registerUser, getUserProfile, updateUserProfile }
