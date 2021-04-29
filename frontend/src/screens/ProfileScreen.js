deb3159d621b924a70ef53f34902dc3131bb1dea
Kundan Patel <kundanpatel@kundans-mbp.myfiosgateway.com>
2021-04-29 02:51:12 -0400

    Final bug fixes

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -7,6 +7,7 @@ import Loader from '../components/Loader'
 import { getUserDetails, updateUserProfile } from '../actions/userActions'
 import { listMyOrders } from '../actions/orderActions'
 import { LinkContainer } from 'react-router-bootstrap'
+import { USER_UPDATE_PROFILE_RESET } from '../constants/userConstants'
 
 const ProfileScreen = ({ location, history }) => {
 	const [ name, setName ] = useState('')
@@ -34,7 +35,8 @@ const ProfileScreen = ({ location, history }) => {
 			if (!userInfo) {
 				history.push('/login')
 			} else {
-				if (!user.name) {
+				if (!user || !user.name || success) {
+					dispatch({ type: USER_UPDATE_PROFILE_RESET })
 					dispatch(getUserDetails('profile'))
 					dispatch(listMyOrders())
 				} else {
@@ -43,7 +45,7 @@ const ProfileScreen = ({ location, history }) => {
 				}
 			}
 		},
-		[ dispatch, history, userInfo, user ]
+		[ dispatch, history, userInfo, user, success  ]
 	)
 
 	const submitHandler = (e) => {
